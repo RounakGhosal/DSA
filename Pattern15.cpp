3c40ee337334e7015c864acefa15c1cc1e82670d
Rounak_Ghosal <161577911+RounakGhosal@users.noreply.github.com>
2026-05-21 20:11:43 +0530

    Add files via upload

---
+++ Pattern15.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char c =65;
+	int n;
+	cout << "Enter value of n: ";
+	cin >> n;
+	for (int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			cout << c;
+		}
+		cout << endl;
+		c++;
+	}
+	return 0;
+}
