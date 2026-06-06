6d2313288a58a109fdf76e3cf406ee7321dc38e7
Rounak_Ghosal <161577911+RounakGhosal@users.noreply.github.com>
2026-06-06 13:43:12 +0530

    Add files via upload

---
+++ LeftRotation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+        reverse(nums.begin(), nums.end());
+    }
+};
+
+int main() {
+    int n, k;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    Solution obj;
+    obj.rotate(nums, k);
+
+    cout << "Left rotated array: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
