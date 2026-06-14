35e222069646f4637ef1ac0aaf5113f891cfc922
Rounak_Ghosal <161577911+RounakGhosal@users.noreply.github.com>
2026-06-14 20:29:53 +0530

    Add files via upload

---
+++ Intersection(Brute).cpp
@@ -0,0 +1,24 @@
+//If two array is not sorted
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    int i=0;
+    int j=0;
+    int n1=nums1.size();
+    int n2=nums2.size();
+    sort(nums1.begin(),nums1.end());
+    sort(nums2.begin(),nums2.end());
+    vector<int> ans;
+    while(i<n1 && j<n2){
+        if (nums1[i]==nums2[j]){
+            if (ans.empty() || ans.back()!=nums1[i]){
+                ans.push_back(nums1[i]);
+            }
+            i++;
+            j++;
+        }
+        else if (nums1[i]<nums2[j])
+            i++;
+        else
+            j++;
+    }
+    return ans;
+}
