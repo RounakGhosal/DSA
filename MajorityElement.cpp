54446ea4d7f845cb875ee118b48141e5718bb0b7
Rounak_Ghosal <161577911+RounakGhosal@users.noreply.github.com>
2026-08-16 12:43:48 +0530

    Add files via upload

---
+++ MajorityElement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int c =0;
+        int el;
+        for (int i=0;i<nums.size();i++){
+            if (c==0){
+                c=1;
+                el=nums[i];
+            }
+            else if (nums[i]==el){
+                c++;
+            }
+            else 
+                c--;
+        }
+        return el;
+    }
+};
